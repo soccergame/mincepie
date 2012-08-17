3aad37dd4c32af0066bb6ce18a26c068c1d7e013
Yangqing Jia <jiayq@eecs.berkeley.edu>
2012-08-17 09:16:38 -0700

    created the package structure

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
+
 setup(
     name='mincepie',
     version='0.0.1',
     description='Simple MapReduce based on Python',
     author='Yangqing Jia',
     author_email='jiayq84@gmail.com',
-    py_modules=['mincepie'],
-    install_requires=[],
+    packages=['mincepie'],
+    install_requires=['python-gflags'],
 )
