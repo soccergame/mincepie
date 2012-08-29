95190b2f01506a56f01f03281bdd7628cf37d05a
Yangqing Jia <jiayq84@gmail.com>
2012-08-28 18:44:23 -0700

    most features I need are there, so let's call it 0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='mincepie',
-    version='0.0.1',
+    version='0.1.0',
     description='Simple MapReduce based on Python',
     author='Yangqing Jia',
     author_email='jiayq84@gmail.com',
