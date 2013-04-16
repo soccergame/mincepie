4a87e30fe859c2c86d10f58ff43e6c9cd685e0ae
Yangqing Jia <jiayq84@gmail.com>
2013-04-16 15:26:59 -0700

    __init__.py update

--- mincepie/__init__.py
+++ mincepie/__init__.py
@@ -1,2 +1,12 @@
-"""Lightweight MapReduce on Python
 """
+Lightweight MapReduce on Python
+
+Yangqing Jia, jiayq@eecs.berkeley.edu
+"""
+__version__ = '0.1'
+
+from . import launcher
+from . import mapreducer
+from . import mince
+
+__all__ = ['launcher', 'mapreducer', 'mince']
